6dfb4d775cc4ca857b9d761289ce836890a77e51
kazimhr <53475165+kazimhr@users.noreply.github.com>
2019-08-15 16:47:28 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ $(window).scroll(function(){
         }
     });
 
-// function chgCol(){
-// 	// document.getElementById("MyElement").classList.add('MyClass');
-// 	document.getElementById("#navBut").classList
+function mobNavBackground(x) {
+  $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll < 100){
+            $('.fixed-top').css('background', 'transparent');
+        } else{
+            $('.fixed-top').css('background', 'black');
+        }
+    });
+}
+
+var x = window.matchMedia("(max-width: 620px)")
+mobNavBackground(x) // Call listener function at run time
+x.addListener(mobNavBackground) // Attach listener function on state changes
+
+// function mobNavColClose(x) {
+ 
 // }
+
+// var z = window.matchMedia("(max-width: 620px)")
+// mobNavColClose(z) // Call listener function at run time
+// z.addListener(mobNavColClose) // Attach listener function on state changes
+
